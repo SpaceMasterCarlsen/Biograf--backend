8d8a109d6e55a74e7243492aa50e81f8795a9657
SpaceMasterCarlsen <96848056+SpaceMasterCarlsen@users.noreply.github.com>
2025-02-28 13:49:34 +0100

    setup

---
+++ src/main/java/biograf/BiografBackendApplication.java
@@ -0,0 +1,13 @@
+package biograf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiografBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiografBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/biograf/BiografBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package biograf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiografBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
