7581dbe40f1923816de6fb023af848b720ea46ee
lucasmodin <lumo0002@stud.kea.dk>
2025-03-05 00:26:06 +0100

    final commit of the day - made getAll controller method for ShowTimeController

--- src/main/java/biograf/controller/ShowTimeController.java
+++ src/main/java/biograf/controller/ShowTimeController.java
@@ -1,9 +1,14 @@
 package biograf.controller;
 
+import biograf.model.ShowTime;
 import biograf.service.ShowTimeService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/showtime")
@@ -14,5 +19,11 @@ public class ShowTimeController {
 
     public ShowTimeController(ShowTimeService showTimeService) {
         this.showTimeService = showTimeService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<ShowTime>> getAllShowTimes() {
+        List<ShowTime> showTimes = showTimeService.getAllShowTime();
+        return ResponseEntity.ok(showTimes);
     }
 }
