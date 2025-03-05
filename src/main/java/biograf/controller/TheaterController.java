b4eab43e640654f46850cecbe2f97095d68a0024
Kristoffer Søholm Gillesberg <kristoffer6400@gmail.com>
2025-03-05 20:15:38 +0100

    Basic CRUD w/o update. No testing done as of yet

--- src/main/java/biograf/controller/TheaterController.java
+++ src/main/java/biograf/controller/TheaterController.java
@@ -1,9 +1,11 @@
 package biograf.controller;
 
+import biograf.model.Theater;
 import biograf.service.TheaterService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/theater")
@@ -15,4 +17,34 @@ public class TheaterController {
     public TheaterController(TheaterService theaterService) {
         this.theaterService = theaterService;
     }
+
+    //To be tested
+    @GetMapping("")
+    public ResponseEntity<List<Theater>> getAllTheaters() {
+        List<Theater> theaters = theaterService.getAllTheaters();
+        return ResponseEntity.ok(theaters);
+    }
+
+    //To be tested
+    @GetMapping("/{theaterId}")
+    public ResponseEntity<Theater> getTheaterById(@PathVariable int theaterId) {
+        return theaterService.getTheaterById(theaterId)
+                .map(theater -> ResponseEntity.ok().body(theater))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    //To be tested
+    @PostMapping("/createTheater")
+    public ResponseEntity<Theater> saveTheater(@RequestBody Theater theater) {
+        Theater savedTheater = theaterService.saveTheater(theater);
+        return ResponseEntity.ok(savedTheater);
+    }
+
+    //To be tested
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTheater(@PathVariable int theaterId) {
+        theaterService.deleteTheater(theaterId);
+        return ResponseEntity.noContent().build();
+    }
+
 }
